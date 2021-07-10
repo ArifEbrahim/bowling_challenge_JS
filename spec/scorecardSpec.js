f9eaada751ffee088832be789211aa18979f9adb
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-07-10 13:24:30 +0100

    added score method

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -8,13 +8,23 @@ describe('Scorecard', () => {
     scorecard = new Scorecard();
   });
 
-  it('can store user rolls', () => {
-    scorecard.roll(4);
-    expect(scorecard.rolls.length).toEqual(1);
+  describe('roll method', () => {
+    it('allows players to make rolls', () => {
+      scorecard.roll(4);
+      expect(scorecard.rolls.length).toEqual(1);
+    });
+  
+    it('throws an error if more than 10 pins knocked down in a roll', () => {
+      expect(() => {scorecard.roll(11);}).toThrowError('More than 10 pins detected, please check your roll')
+    });
   });
 
-  it('throws an error if more than 10 pins knocked down in a roll', () => {
-    expect(() => {scorecard.roll(11);}).toThrowError('More than 10 pins detected, please check your roll')
+
+
+  describe('score method', () => {
+    it('has a score method', () => {
+      expect(scorecard.score).toBeDefined();
+    });
   });
 
 
