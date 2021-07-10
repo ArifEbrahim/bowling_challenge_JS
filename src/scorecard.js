744ced3713980968e08ca773432850672eb5c42e
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-07-10 16:47:04 +0100

    refactor

--- src/scorecard.js
+++ src/scorecard.js
@@ -10,24 +10,42 @@ class Scorecard {
     if(pins > 10) {
       throw new Error('More than 10 pins detected, please check your roll');
     } else if (pins === 10) {
-      this.rolls.push(pins);
-      this.rolls.push(0);
+      this._storeStrike(pins);
     } else {
-      this.rolls.push(pins);
+      this._storeRoll(pins);
     };
   };
 
   score = () => {
     for(let i = 0; i < 20; i+=2) {
-      if (this.rolls[i] === 10) {
-        this.total += this.rolls[i] + this.rolls[i+2] + this.rolls[i+3];
-      } else if(this.rolls[i] + this.rolls[i+1] === 10) {
-        this.total += this.rolls[i] + this.rolls[i+1] + this.rolls[i+2];
+      if (this._isStrike(i)) {
+        this._strikeBonus(i);
+      } else if(this._isSpare(i)) {
+        this._spareBonus(i);
       } else {
-        this.total += this.rolls[i] + this.rolls[i+1];
+        this._normalScore(i);
       };
     };
     return this.total;
+  };
+
+  // private methods 
+
+  _storeRoll = (pins) => this.rolls.push(pins);
+
+  _isStrike = (i) => this.rolls[i] === 10;
+
+  _strikeBonus = (i) => this.total += this.rolls[i] + this.rolls[i+2] + this.rolls[i+3];
+
+  _isSpare = (i) => this.rolls[i] + this.rolls[i+1] === 10;
+
+  _spareBonus = (i) => this.total += this.rolls[i] + this.rolls[i+1] + this.rolls[i+2];
+
+  _normalScore = (i) => this.total += this.rolls[i] + this.rolls[i+1];
+
+  _storeStrike = (pins) => {
+    this._storeRoll(pins);
+    this._storeRoll(0);
   };
 
 };
